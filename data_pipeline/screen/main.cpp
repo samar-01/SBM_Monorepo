5816227dce005aac9bbf07e9e88e70f3d5527e37
samar-01 <66644907+samar-01@users.noreply.github.com>
2023-01-27 18:14:58 -0500

    dataset and option to choose custom or default font

--- data_pipeline/screen/main.cpp
+++ data_pipeline/screen/main.cpp
@@ -1,19 +1,59 @@
 #include "raylib.h"
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
 
+// #define CUSTOM_FONT
+#define TEST_MODE
+
+class Data {
+public:
+	int mph;
+	int fbp;
+	int rbp;
+	int throttle;
+	int brake;
+	int steer;
+	int cvtTemp;
+	int engineTemp;
+	void init();
+	#ifdef TEST_MODE
+	void randomize();
+	#endif
+private:
+	vector<int*> datapoints;
+} testData;
 void updateData();
 
+void text(char* text, Vector2 pos);
+void text(char* text, Vector2 pos, int size);
+
+#ifdef CUSTOM_FONT
+Font font;
+#endif
+const int fontSize = 30;
+const int spacing = 2;
+char* bottomText = "SBU Motorsports";
+const int bottomFontSize = 50;
+
 int main(void) {
+	SetRandomSeed(0);
 	const int screenWidth = 800;
 	const int screenHeight = 480;
 
 	InitWindow(screenWidth, screenHeight, "SBU Motorsports");
 	SetTargetFPS(60);
+
 	Texture2D texture = LoadTexture("resources/SBMLogo.png");
-	const int fontSize = 50;
-	Font font = LoadFontEx("resources/font.ttf", fontSize, 0, 250);
-	const int spacing = 2;
-	const char* bottomText = "SBU Motorsports";
-	Vector2 bottomSize = MeasureTextEx(font, bottomText, fontSize, spacing);
+	#ifdef CUSTOM_FONT
+	font = LoadFontEx("resources/font.ttf", fontSize, 0, 250);
+	Vector2 bottomSize = MeasureTextEx(font, bottomText, bottomFontSize, spacing);
+	#else
+	Vector2 bottomSize = (Vector2){ MeasureText(bottomText, bottomFontSize),bottomFontSize };
+	#endif
+
+	testData.init();
 
 	#ifdef __arm__
 	if (!IsWindowFullscreen()) {
@@ -24,17 +64,47 @@ int main(void) {
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(BLACK);
+		DrawFPS(0, 0);
 		DrawTexture(texture, 0, screenHeight - texture.height - bottomSize.y, WHITE);
-		DrawTextEx(font, bottomText, (Vector2) { (screenWidth - bottomSize.x) / 2, screenHeight - bottomSize.y }, fontSize, spacing, WHITE);
+		text(bottomText, (Vector2) { (screenWidth - bottomSize.x) / 2, screenHeight - bottomSize.y }, bottomFontSize);
+		text(bottomText, (Vector2) { 0, 0 });
 		EndDrawing();
+		updateData();
 	}
 
 	UnloadTexture(texture);
+	#ifdef CUSTOM_FONT
 	UnloadFont(font);
+	#endif
 	CloseWindow();
 	return 0;
 }
 
-void updateData(){
-	
+void updateData() {
+	testData;
+}
+
+void text(char* inText, Vector2 pos, int size) {
+	#ifdef CUSTOM_FONT
+	DrawTextEx(font, inText, pos, size, spacing, WHITE);
+	#else
+	DrawText(inText, pos.x, pos.y, size, WHITE);
+	#endif
+}
+
+void text(char* inText, Vector2 pos) {
+	text(inText, pos, fontSize);
+}
+
+void Data::init() {
+	datapoints = { &mph, &fbp, &rbp, &throttle, &brake, &steer, &cvtTemp, &engineTemp };
+	for (int i = 0; i < datapoints.size(); i++) {
+		*datapoints.at(i) = 0;
+	}
+}
+
+void Data::randomize() {
+	for (int i = 0; i < datapoints.size(); i++) {
+		*datapoints.at(i) = clamp(*datapoints.at(i) + GetRandomValue(-1, 1), 0, 30);
+	}
 }
