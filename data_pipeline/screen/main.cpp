07bdcbf784aa05dea2d6b2dd0344db338b05c98e
samar-01 <66644907+samar-01@users.noreply.github.com>
2023-01-27 16:52:46 -0500

    auto full screen when compiled with raspberry pi

--- data_pipeline/screen/main.cpp
+++ data_pipeline/screen/main.cpp
@@ -9,14 +9,20 @@ int main(void) {
 	Texture2D texture = LoadTexture("resources/SBMLogo.png");
 	Font fontTtf = LoadFontEx("resources/font.ttf", 32, 0, 250);
 
+	#ifdef __arm__
+	if (!IsWindowFullscreen()){
+		ToggleFullscreen();
+	}
+	#endif
+
 	while (!WindowShouldClose()){
 		BeginDrawing();
 		ClearBackground(BLACK);
-		DrawTextEx(fontTtf, "hello", (Vector2){0.0f,0.0f}, 30.0f, 2, WHITE);
+		DrawTextEx(fontTtf, "hello12345", (Vector2){0.0f,0.0f}, 30.0f, 2, WHITE);
 		DrawTexture(texture, 0, screenHeight - texture.height, WHITE);
 		EndDrawing();
 	}
-	
+
 	UnloadTexture(texture);
 	UnloadFont(fontTtf);
 	CloseWindow();
